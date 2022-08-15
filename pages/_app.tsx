2e45573f08105fe21c6412f91ff9c13efd052574
Nicholas Hallahan <nicholas.hallahan@veckta.com>
2022-08-15 16:59:43 -0700

    Added Joy UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import { SessionProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
+import { CssVarsProvider } from '@mui/joy/styles'
+import Button from '@mui/joy/Button'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <CssVarsProvider>
+        <Component {...pageProps} />
+        <Button>Joy UI</Button>
+      </CssVarsProvider>
     </SessionProvider>
   )
 }
